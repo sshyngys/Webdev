1b812adc14d7916e3a5e790b022a7e5111e33f61
sshyngys <78813731+sshyngys@users.noreply.github.com>
2021-03-01 19:36:54 +0600

    lab5

---
+++ w5/src/app/categories.ts
@@ -0,0 +1,18 @@
+export const categories = [
+  {
+    id: 1,
+    name: 'Computer Components'
+  },
+  {
+    id: 2,
+    name: 'Computer Accessories'
+  },
+  {
+    id: 3,
+    name: 'Computers & Tablets'
+  },
+  {
+    id: 4,
+    name: 'Networking Products'
+  }
+]
