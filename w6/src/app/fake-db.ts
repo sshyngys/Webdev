c685f0e7bfb8eac922aef3153443322689ce380b
sshyngys <78813731+sshyngys@users.noreply.github.com>
2021-03-09 21:59:30 +0600

    lab6

---
+++ w6/src/app/fake-db.ts
@@ -0,0 +1,7 @@
+import {Album} from './models';
+
+export const ALBUM: Album[] = [
+  { userId: 1, id: 1, title: 'title 1'},
+  { userId: 1, id: 2, title: 'title 2'},
+  { userId: 1, id: 3, title: 'title 3'}
+];
